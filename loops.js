d96321c55ca4d00276a2802c09450ba0e4ef5396
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-18 16:06:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,10 +16,15 @@ function whileLoop(n) {
 return "done"
 }
 
+function doWhileLoop(num)
+
 
 // Define a function called doWhileLoop in loops.js. The function should take an integer as an argument. Use the incrementVariable() function (you can copy it from this README) in the condition, and console log "I run once regardless." while incrementVariable() returns a number less than the parameter received. (Your condition might look something like incrementVariable() < num.) Remember that it should also console log when receiving 0 as a parameter because the do-while runs before the condition is checked
 // 1) loops
        doWhileLoop(num)
          console logs "I run once regardless." 1 time when passed an integer of0 as a parameter.:
      ReferenceError: doWhileLoop is not defined
-// 
+// 2) loops
+       doWhileLoop(num)
+         console logs "I run once regardless." 10 times when passed an integer of 10 as a parameter.:
+     ReferenceError: doWhileLoop is not defined
