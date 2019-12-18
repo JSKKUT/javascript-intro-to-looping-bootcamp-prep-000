2b0c92165657ef53dd180a0b4e0c77526fc10ee9
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-18 17:32:18 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -31,13 +31,3 @@ function doWhileLoop(num) {
     console.log("I run once regardless.");
   } while (incrementVariable() <= num);
 }
-
-// 1) loops
-       doWhileLoop(num)
-         console logs "I run once regardless." 1 time when passed an integer of0 as a parameter.:
-     ReferenceError: doWhileLoop is not defined
-// 2) loops
-       doWhileLoop(num)
-         console logs "I run once regardless." 10 times when passed an integer of 10 as a parameter.:
-     ReferenceError: doWhileLoop is not defined
-     
